589db30ee913122bffcce6113392bd4389b30029
Gabor Szabo <gabor@szabgab.com>
2023-01-29 23:28:49 +0200

    move code inside function

--- ladino/ladinadores.py
+++ ladino/ladinadores.py
@@ -1,25 +1,31 @@
 import os
 from yaml import safe_load
 
-root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-#print(root)
-with open(os.path.join(root, 'afishes.yaml')) as fh:
-    data = safe_load(fh)
-images_dir = os.path.join(root, 'docs', 'afishes')
+def load_ladinadores(root):
+    with open(os.path.join(root, 'afishes.yaml')) as fh:
+        data = safe_load(fh)
+    images_dir = os.path.join(root, 'docs', 'afishes')
 
-images = set(os.listdir(images_dir))
+    images = set(os.listdir(images_dir))
 
-images_in_yaml = set()
+    images_in_yaml = set()
 
-for entry in data:
-    assert 'title' in entry
-    assert len(entry['title']) > 5
-    assert 'img' in entry
-    assert os.path.exists(os.path.join(images_dir, entry['img']))
-    images_in_yaml.add(entry['img'])
+    for entry in data:
+        assert 'title' in entry
+        assert len(entry['title']) > 5
+        assert 'img' in entry
+        assert os.path.exists(os.path.join(images_dir, entry['img']))
+        images_in_yaml.add(entry['img'])
 
-if images != images_in_yaml:
-    print(images-images_in_yaml)
-    exit(1)
+    if images != images_in_yaml:
+        print(images-images_in_yaml)
+        exit(1)
 
-print("Everything looks fine")
+    return data
+
+if __name__ == "__main__":
+    root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    #print(root)
+    load_ladinadores(root)
+    print("Everything looks fine")
+
